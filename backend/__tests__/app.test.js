e75d2d66f41fb2ba3af2e0c048305be9c109bcaa
L G Harishree <harishreelg.22msc@kongu.edu>
2025-05-13 13:26:38 +0530

    Update app.test.js

--- backend/__tests__/app.test.js
+++ backend/__tests__/app.test.js
@@ -1,9 +1,1 @@
-const request = require('supertest');
-const app = require('../app');  // Assuming your Express app is exported from app.js
 
-describe('Recipe API Tests', () => {
-  test('GET / should return 200', async () => {
-    const response = await request(app).get('/');
-    expect(response.statusCode).toBe(200);
-  });
-});
